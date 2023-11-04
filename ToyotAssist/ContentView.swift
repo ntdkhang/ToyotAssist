a50900bd29ad29e1e2bdd9c74bedd97ae1022a00
ntdkhang <ntdkhang143@gmail.com>
2023-11-04 14:30:18 -0500

    Initial Commit

---
+++ ToyotAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToyotAssist
+//
+//  Created by Khang Nguyen on 11/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToyotAssist/ToyotAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyotAssistApp.swift
+//  ToyotAssist
+//
+//  Created by Khang Nguyen on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ToyotAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
